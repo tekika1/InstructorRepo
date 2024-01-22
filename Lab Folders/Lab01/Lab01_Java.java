8700e75bd2a61b2d4a38d1feacd7e240cd1572a8
tekika1 deiter <ephraimaadefuye@outlook.com>
2024-01-22 15:53:44 +0000

    test comment

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -5,6 +5,7 @@ void testMethod1()
   //First command x = y + z 
   //Second commnad print(x)
   // Edit made by Ephraim_Adefuye_220253804
+  //Edit by Ephraim_Adefuye_220253804 in Gitpod
 
 
   //Last line print('TA TA')
